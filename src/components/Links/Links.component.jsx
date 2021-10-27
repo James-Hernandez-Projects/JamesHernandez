75c1e406b0c9a72d04286831adc48bef2d87c1fd
James Hernandez <happjnh@gmail.com>
2021-10-27 00:00:09 -0700

    fixed links

--- src/components/Links/Links.component.jsx
+++ src/components/Links/Links.component.jsx
@@ -8,14 +8,14 @@ const Links = () => {
                 target='_blank'>
                 Linkedin
                 </a>
-                <a href="www.linkedin.com/in/james-hernandez-1ab831204" className="button-appearence" style={{backgroundColor:'#FFB830'}}
+                <a href="https://github.com/James-Hernandez-Projects" className="button-appearence" style={{backgroundColor:'#FFB830'}}
                 target='_blank'>
                 Github
                 </a>
                 <a href="mailto:happjnh@gmail.com" className="button-appearence" style={{backgroundColor:'#FF2442'}}>
                 Email
                 </a>
-                <a href="www.linkedin.com/in/james-hernandez-1ab831204" className="button-appearence" style={{backgroundColor:'#79D70F'}}
+                <a href="https://james-hernandez-projects.github.io/resume/" className="button-appearence" style={{backgroundColor:'#79D70F'}}
                 target='_blank'>
                 Resume
                 </a>
